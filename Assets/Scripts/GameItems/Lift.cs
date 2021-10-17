d774b702d992c6330da83df67adc5e021ff5729d
Thomas Lamb <s200498@students.aie.edu.au>
2021-10-17 20:13:49 +1100

    Lift implemented

--- Assets/Scripts/GameItems/Lift.cs
+++ Assets/Scripts/GameItems/Lift.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 using Malicious.Core;
 using UnityEditor;
@@ -8,22 +9,15 @@ namespace Malicious.GameItems
     public class Lift : MonoBehaviour
     {
         [SerializeField] private float _moveSpeed = 0.5f;
-        private Vector3 _targetLocation = Vector3.zero;
-
         [SerializeField] private Vector3 _topPosition = Vector3.zero;
         [SerializeField] private Vector3 _groundPosition = Vector3.zero;
+        [SerializeField] private Transform _liftObject = null;
         
         private bool _wait = true;
-        
         private bool _moveUp = false;
         private bool _moveDown = false;
-
         private float _timer = 0;
         
-        void Start()
-        {
-            _targetLocation = _topPosition;
-        }
         void FixedUpdate()
         {
             if (_wait)
@@ -31,26 +25,70 @@ namespace Malicious.GameItems
 
             if (_moveUp)
             {
-                _timer += Progress.TimeDisplayMode.
+                _timer += Time.deltaTime * _moveSpeed;
+                _liftObject.position = Vector3.Lerp(_groundPosition, _topPosition, _timer);
             }
             else if (_moveDown)
             {
-                
+                _timer += Time.deltaTime * _moveSpeed;
+                _liftObject.position = Vector3.Lerp(_topPosition,_groundPosition,  _timer);
+            }
+
+            if (_timer >= 1)
+            {
+                _timer = 0;
+                _wait = true;
             }
         }
 
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                other.gameObject.transform.parent = _liftObject.transform;
+            }
+        }
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                other.gameObject.transform.parent = null;
+            }
+        }
+
+        [ContextMenu("SetPositions")]
+        public void SetPositions()
+        {
+            _groundPosition = _liftObject.position;
+            _topPosition = _groundPosition;
+            _topPosition.y += 3;
+        }
+
+
+        [ContextMenu("MoveUp")]
         public void MoveUp()
         {
             _moveUp = true;
             _moveDown = false;
             _timer = 0;
+            _wait = false;
         }
 
+        [ContextMenu("MoveDown")]
         public void MoveToGround()
         {
             _moveUp = false;
             _moveDown = true;
             _timer = 0;
+            _wait = false;
         }
+        
+        #if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawCube(_groundPosition, Vector3.one);
+            Gizmos.DrawCube(_topPosition, Vector3.one);
+        }
+#endif
     }
 }
