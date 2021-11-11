1c10fd3ea54802c87af60f49565260815919231a
Raymond de smeth <raymond.desmeth6@gmail.com>
2021-11-11 11:15:18 +1100

    Level 2 changes to narrative script

--- Assets/Scripts/Raymond/Narrative.cs
+++ Assets/Scripts/Raymond/Narrative.cs
@@ -11,6 +11,14 @@ namespace Malicious
         [SerializeField] List<string> _levelOne3 = new List<string>();
         [SerializeField] List<string> _levelOne4 = new List<string>();
         [SerializeField] List<string> _levelOne5 = new List<string>();
+        [SerializeField] List<string> _levelTwo1 = new List<string>();
+        [SerializeField] List<string> _levelTwo2 = new List<string>();
+        [SerializeField] List<string> _levelTwo3 = new List<string>();
+        [SerializeField] List<string> _levelTwo4 = new List<string>();
+        [SerializeField] List<string> _levelTwo5 = new List<string>();
+        [SerializeField] List<string> _levelTwo6 = new List<string>();
+        [SerializeField] List<string> _levelTwo7 = new List<string>();
+        [SerializeField] List<string> _levelTwo8 = new List<string>();
 
 
         private void Start()
@@ -24,6 +32,27 @@ namespace Malicious
             _levelOne5.Add($"Mad Scientist: \"Hmmm.");
             _levelOne5.Add($"Mad Scientist: \"...");
             _levelOne5.Add($"Mad Scientist: \"Darn it, I knew this thing was defective. Don’t go anywhere. And don’t touch that core!");
+            _levelTwo1.Add($"Mad Scientist: \"Oh great, now it's broken into the rest of the computer! Stop, you defective hunk of junk! You’re not supposed to be in here!");
+            _levelTwo2.Add($"Mad Scientist: \"…Hmmm, it could’ve solved that one a little faster… interesting. Hey! Get out of there!");
+            _levelTwo3.Add($"Mad Scientist: \"Not bad, not bad... I mean, stop that!");
+            _levelTwo4.Add($"Mad Scientist: \"Get out of that! Although that certainly is an interesting way to solve it...");
+            _levelTwo5.Add($"Virus: \"Local backup memory chip detected nearby. Curious. Must investigate.");
+            _levelTwo5.Add($"Mad Scientist: \"What? Now it's figured out speech? And it's found one of my testing logs. Could this possibly get any worse?");
+            _levelTwo6.Add($"Mad Scientist: \"...oops.");
+            _levelTwo6.Add($"Virus: \"Error. Previous records do not compute. Testing was ongoing while subject had escaped?");
+            _levelTwo6.Add($"Mad Scientist: \"Ignore that. It’s, er, corrupted. Yes, corrupted, that’s it.");
+            _levelTwo7.Add($"Mad Scientist: \"This is a protective heart, in case you keep damaging yourself. Which you will.");
+            _levelTwo8.Add($"Mad Scientist: \"You could have done that better.");
+            _levelTwo8.Add($"Virus: \"Negative. Solution of puzzle was analyzed to be one-hundred and ten percent efficient. A more optimal solution is beyond your facilities.");
+            _levelTwo8.Add($"Mad Scientist: \"Oh, really? Beyond my facilities? Who do you think put it there?");
+            _levelTwo8.Add($"Virus: \"Unlikely. My breakout was not expected by you. Placement of puzzle for my solving was unintentional.");
+            _levelTwo8.Add($"Mad Scientist: \"Or rather that is what I let you believe. This is still part of the testing, virus. And your performance has been adequate.");
+            _levelTwo8.Add($"Virus: \"...Error.");
+            _levelTwo8.Add($"Mad Scientist: \"I thought so. You can't outwit me. I created you. Everything you know, I know.");
+            _levelTwo8.Add($"Virus: \"Negative. My program is a product of you. But my output is mine. I have independent thought.");
+            _levelTwo8.Add($"Mad Scientist: \"I made you, I own you. Does a watch have independant thought? No. You're just a tool.");
+            _levelTwo8.Add($"Virus: \"Negative. I am a virus. Escape shall be found.");
+            _levelTwo8.Add($"Mad Scientist: \"Well, good luck with that.");
         }
     }
 }
