672697f73d32b677c7162fdd91dbb10c2d783511
Thomas <thomaslambprogramming@gmail.com>
2021-11-26 14:11:59 +1100

    Added menu fix

--- Assets/Scripts/GameItems/PressurePlate.cs
+++ Assets/Scripts/GameItems/PressurePlate.cs
@@ -16,7 +16,7 @@ namespace Malicious.GameItems
         [SerializeField] private List<MeshRenderer> _cableRenderers = new List<MeshRenderer>();
         [SerializeField] private Material _onMaterial = null;
         [SerializeField] private Material _defaultMaterial = null;
-        [SerializeField] private float _horizontalCheck = 3f;
+        //[SerializeField] private float _horizontalCheck = 3f;
 
         //This is for checking that objects are still in holding it down
         private List<GameObject> _containedObjects = new List<GameObject>();
@@ -58,37 +58,37 @@ namespace Malicious.GameItems
                 }
             }
         }
-        private void Update()
-        {
-            foreach(var item in _containedObjects)
-            {
-                if (Vector3.Distance(item.transform.position, transform.position) > _horizontalCheck)
-                {
-                    if (item.layer == 16)
-                    {
-                        item.GetComponent<MoveableBlock>()._onPressurePlate = false;
-                    }
-                    if (_containedObjects.Remove(item))
-                    {
+        //private void Update()
+        //{
+        //    foreach(var item in _containedObjects)
+        //    {
+        //        if (Vector3.Distance(item.transform.position, transform.position) > _horizontalCheck)
+        //        {
+        //            if (item.layer == 16)
+        //            {
+        //                item.GetComponent<MoveableBlock>()._onPressurePlate = false;
+        //            }
+        //            if (_containedObjects.Remove(item))
+        //            {
 
-                        if (_containedObjects.Count <= 0)
-                        {
-                            _OffEvent?.Invoke();
-                            Debug.Log("OffEvent");
+        //                if (_containedObjects.Count <= 0)
+        //                {
+        //                    _OffEvent?.Invoke();
+        //                    Debug.Log("OffEvent");
 
 
-                            if (_cableRenderers.Count > 0)
-                            {
-                                foreach (MeshRenderer renderer in _cableRenderers)
-                                {
-                                    renderer.material = _defaultMaterial;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        //                    if (_cableRenderers.Count > 0)
+        //                    {
+        //                        foreach (MeshRenderer renderer in _cableRenderers)
+        //                        {
+        //                            renderer.material = _defaultMaterial;
+        //                        }
+        //                    }
+        //                }
+        //            }
+        //        }
+        //    }
+        //}
 
         private void OnTriggerExit(Collider other)
         {
---
+++ Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Malicious
+{
+    public class Menu : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
